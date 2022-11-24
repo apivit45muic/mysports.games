a1c5d79415c34423f1b03470ba448f6a2266d049
gaylordfucker007 <91743080+gaylordfucker007@users.noreply.github.com>
2022-11-24 22:51:15 +0700

    Add Starter File (Springboot setup/ Pre-start dependencies)

---
+++ src/test/java/io/muzoo/ssc/mysports/MysportsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.muzoo.ssc.mysports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
